6f60ae425bd59e56d1447498991f889a5d13f3be
Michel Roca <mroca.dh@gmail.com>
2013-08-29 19:37:09 +0200

    First tests

---
+++ test/helpers/httpHelper.js
@@ -0,0 +1,40 @@
+var http = require('http');
+
+function http_get(host, port, url, cb) {
+    var options = {
+        host  : host,
+        port  : port,
+        path  : url,
+        method: 'GET'
+    };
+    var ret = false;
+    var req = http.request(options, function (res) {
+        var buffer = '';
+        res.on('data', function (data) {
+            buffer += data;
+        });
+        res.on('end', function () {
+            cb(null, res, buffer);
+        });
+    });
+    req.end();
+    req.on('error', function (e) {
+        if ( !ret ) {
+            cb(e, null, null);
+        }
+    });
+}
+
+var client = {
+    host: "",
+    port: "",
+    get: function (url, cb) {
+        http_get(this.host, this.port, url, cb);
+    }
+};
+
+var httpHelper = {
+    client: client
+};
+
+module.exports = httpHelper;
---
+++ test/routesTest.js
@@ -0,0 +1,26 @@
+var httpHelper = require('./helpers/httpHelper.js');
+var should = require("should");
+var server = require("../server.js");
+
+httpHelper.client.host = "localhost";
+httpHelper.client.port = "3000";
+
+describe("When testing routes", function () {
+
+    it('GET / should return 200', function (done) {
+        httpHelper.client.get('/', function (err, res, body) {
+            if ( err ) err.should.equal(null);
+            res.statusCode.should.equal(200);
+            done();
+        });
+    });
+
+    it('GET /hello should return 200 and contain "Hello World"', function (done) {
+        httpHelper.client.get('/hello', function (err, res, body) {
+            if ( err ) err.should.equal(null);
+            res.statusCode.should.equal(200);
+            body.should.include("Hello World");
+            done();
+        });
+    });
+});
