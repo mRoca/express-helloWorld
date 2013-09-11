131bbe04de0105227129c418165ea025baa19fc2
Michel Roca <mroca.dh@gmail.com>
2013-09-11 14:57:08 +0200

    Add requests logs

--- server.js
+++ server.js
@@ -3,12 +3,17 @@ var express = require('express');
 
 var app = express();
 
+app.use(function(req, res, next){
+    console.log('%s %s', req.method, req.url);
+    next();
+});
+
 app.get('/', function(req, res){
-    res.send('Hello you! <a href="hello">Click here</a>');
+    res.send('Hello you! <a href="hello">Click here to go to the /hello page</a>');
 });
 
 app.get('/hello', function(req, res){
-    res.send('Hello World <a href="hello.txt">Click here</a>');
+    res.send('Hello World <a href="hello.txt">Click here to display the hello.txt file</a>');
 });
 
 app.get('/hello.txt', function(req, res){
